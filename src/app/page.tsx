7dd5ac6d92a4ffccfd000f4e490a6196d7e84629
KevC000 <chenkevin1119@gmail.com>
2024-03-31 02:53:01 -0400

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <HeroSection />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,19 +1,77 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
-type Props = {};
-
-const HeroSection = (props: Props) => {
-	return (
-    <section>
-      <h1 className="text-white mb-4 text-6xl font-extrabold">
-        Hello, I &apos;m Kevin Chen
-      </h1>
-      <p className="text-[#ADB7BE]">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
-        distinctio magni fuga, aperiam maxime ea ex necessitatibus nesciunt eos
-        pariatur molestiae hic inventore accusamus quidem aliquam dolorem vero
-        obcaecati omnis
-      </p>
+const HeroSection = () => {
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 w-full place-self-center text-center sm:text-left"
+        >
+          <h1 className="text-white max-w-2xl mb-4 lg:text-6xl text-4xl font-extrabold">
+            <span className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Kevin Chen",
+                1000,
+                "Android Developer",
+                1000,
+                "Web Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] mb-6 textl-lg lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptatum.
+          </p>
+          <div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full object-cover overflow-hidden bg-[#181818] w-[250px] h-[250px] lg:w-[375px] lg:h-[375px] relative">
+            <Image
+              src="/kevin_chen.jpg"
+              alt="hero image"
+              className="absolute transform -translate-x-1/3 -translate-y-1/3 top-1/2 left-1/3 "
+              width={300}
+              height={300}
+            />
+          </div>
+        </motion.div>
+      </div>
     </section>
   );
 };
