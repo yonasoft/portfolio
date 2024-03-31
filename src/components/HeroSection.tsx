a32b02c21226f6a72ccfd733e76422a3aa14e1f5
KevC000 <chenkevin1119@gmail.com>
2024-03-30 21:43:50 -0400

    initial

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type Props = {};
+
+const HeroSection = (props: Props) => {
+	return (
+    <section>
+      <h1 className="text-white mb-4 text-6xl font-extrabold">
+        Hello, I &apos;m Kevin Chen
+      </h1>
+      <p className="text-[#ADB7BE]">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
+        distinctio magni fuga, aperiam maxime ea ex necessitatibus nesciunt eos
+        pariatur molestiae hic inventore accusamus quidem aliquam dolorem vero
+        obcaecati omnis
+      </p>
+    </section>
+  );
+};
+
+export default HeroSection;
