bf2a5977ce02cbeee941094f27d54ea195326618
KevC000 <chenkevin1119@gmail.com>
2024-04-01 19:17:30 -0400

    adjusted email api

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -7,24 +7,29 @@ const fromEmail = process.env.FROM_EMAIL;
 export const POST = async (req: any, res: any) => {
   const { email, subject, message } = await req.json();
 
-  const receiver = "chenkevin1119@gmail.com";
-  const toEmails: string[] = [email, receiver];
+  const toEmails: string[] = [
+    email,
+    "chenkevin1119@gmail.com",
+    "yonasoft7@gmail.com",
+  ];
 
   try {
     console.log("Sending email...", email, subject, message);
     const data = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
+      from: fromEmail as string,
       to: toEmails,
       subject: subject,
-      text: `Sent From: ${email} to ${receiver}\n 
+      text: `Sent From: ${email}\n 
       Thank you for reaching out! \n
       Subject: ${subject} \n
       Message: ${message}`,
     });
     const resp = NextResponse.json(data);
+    console.log(resp);
     return resp;
   } catch (error) {
     const e = NextResponse.json({ error });
+    console.log(e);
     return e;
   }
 };
