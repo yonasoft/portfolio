e9728cf81705e4a156f7bea6fcf2457f037265a8
Kevin Chen <chenkevin1119@gmail.com>
2025-04-05 22:47:53 -0400

    added google play link

--- src/components/EmailSection.tsx
+++ src/components/EmailSection.tsx
@@ -1,9 +1,9 @@
-"use client";
-import Error from "next/error";
-import Link from "next/link";
-import React, { useState } from "react";
-import { FaGithub, FaLinkedin, FaLink } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+'use client';
+import Error from 'next/error';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { FaGithub, FaLinkedin, FaLink, FaGooglePlay,  } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 type Props = {};
 
@@ -18,15 +18,15 @@ const EmailSection = (props: Props) => {
       message: e.target.message.value,
     };
     const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
+    const endpoint = '/api/send';
 
     // Form the request for sending data to the server.
     const options = {
       // The method is POST because we are sending data.
-      method: "POST",
+      method: 'POST',
       // Tell the server we're sending JSON.
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       // Body of the request is the JSON data we created above.
       body: JSONdata,
@@ -36,97 +36,103 @@ const EmailSection = (props: Props) => {
     const resData = await response.json();
 
     if (response.status === 200) {
-      console.log("Message sent.");
+      console.log('Message sent.');
       setEmailSubmitted(true);
     }
   };
 
   return (
     <section
-      id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+      id='contact'
+      className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'
     >
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-      <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">
+      <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2'></div>
+      <div className='z-10'>
+        <h5 className='text-xl font-bold text-white my-2'>
           Let&apos;s Connect
         </h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md">
+        <p className='text-[#ADB7BE] mb-4 max-w-md'>
           I&apos;m currently looking for opportunities, so my inbox is open.
           Whether you have a question of want to say hi, I&apos;ll do my best to
           get back to you!
         </p>
-        <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/yonasoft" target="_blank">
-            <FaGithub size={36} color="white" />
+        <div className='socials flex flex-row gap-2'>
+          <Link
+            href='https://play.google.com/store/apps/dev?id=4758788971500879272'
+            target='_blank'
+          >
+            <FaGooglePlay size={36} color='white' />
           </Link>
-          <Link href="https://www.linkedin.com/in/yonasoft/" target="_blank">
-            <FaLinkedin size={36} color="white" />
+          <Link href='https://github.com/yonasoft' target='_blank'>
+            <FaGithub size={36} color='white' />
           </Link>
-          <Link href="mailto:yonasoft7@gmail.com" target="_blank">
-            <MdEmail size={36} color="white" />
+          <Link href='https://www.linkedin.com/in/yonasoft/' target='_blank'>
+            <FaLinkedin size={36} color='white' />
           </Link>
-          <Link href="https://linktr.ee/yonasoft" target="_blank">
-            <FaLink size={36} color="white" />
+          <Link href='mailto:yonasoft7@gmail.com' target='_blank'>
+            <MdEmail size={36} color='white' />
+          </Link>
+          <Link href='https://linktr.ee/yonasoft' target='_blank'>
+            <FaLink size={36} color='white' />
           </Link>
         </div>
       </div>
       <div>
         {emailSubmitted ? (
-          <p className="text-green-500 text-sm mt-2">
+          <p className='text-green-500 text-sm mt-2'>
             Email sent successfully!
           </p>
         ) : (
-          <form className="flex flex-col" onSubmit={handleSubmit}>
-            <div className="mb-6">
+          <form className='flex flex-col' onSubmit={handleSubmit}>
+            <div className='mb-6'>
               <label
-                htmlFor="email"
-                className="text-white block mb-2 text-sm font-medium"
+                htmlFor='email'
+                className='text-white block mb-2 text-sm font-medium'
               >
                 Your email
               </label>
               <input
-                name="email"
-                type="email"
-                id="email"
+                name='email'
+                type='email'
+                id='email'
                 required
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="john@google.com"
+                className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                placeholder='john@google.com'
               />
             </div>
-            <div className="mb-6">
+            <div className='mb-6'>
               <label
-                htmlFor="subject"
-                className="text-white block text-sm mb-2 font-medium"
+                htmlFor='subject'
+                className='text-white block text-sm mb-2 font-medium'
               >
                 Subject
               </label>
               <input
-                name="subject"
-                type="text"
-                id="subject"
+                name='subject'
+                type='text'
+                id='subject'
                 required
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Just saying hi"
+                className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                placeholder='Just saying hi'
               />
             </div>
-            <div className="mb-6">
+            <div className='mb-6'>
               <label
-                htmlFor="message"
-                className="text-white block text-sm mb-2 font-medium"
+                htmlFor='message'
+                className='text-white block text-sm mb-2 font-medium'
               >
                 Message
               </label>
               <textarea
-                name="message"
-                id="message"
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                name='message'
+                id='message'
+                className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
                 placeholder="Let's talk about..."
               />
             </div>
             <button
-              type="submit"
-              className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+              type='submit'
+              className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'
             >
               Send Message
             </button>
